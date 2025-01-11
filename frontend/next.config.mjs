255f4707ce4d07dc5f923aa913b21f212249054e
DaehuiG <daehui_go@gm.gist.ac.kr>
2025-01-11 20:01:30 +0900

    URLManagement 5th

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
-import URLManagement from '@/utils/URLManagement';
+const getBaseURL = () => {
+  const baseURLs = {
+    development: `http://localhost:8000`, // 개발 환경
+    production: `http://54.152.161.95:3000`, // 프로덕션 환경
+  };
 
-const url = URLManagement();
+  // NODE_ENV 값에 따라 URL 반환, 기본값은 개발 환경
+  return baseURLs[process.env.NODE_ENV] || baseURLs.development;
+};
+
+const url = getBaseURL();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,7 +17,7 @@ const nextConfig = {
       return [
         {
           source: "/api/:path*/",
-          destination: `${url}/api/:path*/`,
+          destination: `${getBaseURL}/api/:path*/`,
         },
         {
           source: "/media/:path*/",
