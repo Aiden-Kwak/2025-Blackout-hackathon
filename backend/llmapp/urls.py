d836a52c6a322ffc4ecf6064b7b8a51f064d8b1e
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-12 03:24:19 +0900

    kill createembeddidshit

--- backend/llmapp/urls.py
+++ backend/llmapp/urls.py
@@ -1,8 +1,8 @@
 # llmapp/urls.py
 from django.urls import path
-from .views import FetchSlackMessagesAPIView, CreateEmbeddingsAPIView
+from .views import FetchSlackMessagesAPIView#, CreateEmbeddingsAPIView
 
 urlpatterns = [
     path("slack/fetch-messages/", FetchSlackMessagesAPIView.as_view(), name="fetch_slack_messages"),
-    path("slack/create-embeddings/", CreateEmbeddingsAPIView.as_view(), name="create_embeddings"),
+    #path("slack/create-embeddings/", CreateEmbeddingsAPIView.as_view(), name="create_embeddings"),
 ]
