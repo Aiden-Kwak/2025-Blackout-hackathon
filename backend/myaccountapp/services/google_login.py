3d492a2fb87d3e1ea539b4a87a440cab43619e4b
DaehuiG <daehui_go@gm.gist.ac.kr>
2025-01-11 21:50:33 +0900

    DEBUG more

--- backend/myaccountapp/services/google_login.py
+++ backend/myaccountapp/services/google_login.py
@@ -16,16 +16,8 @@ class GoogleRawLoginFlowService:
     def __init__(self):
         self.client_id = settings.GOOGLE_OAUTH2_CLIENT_ID
         self.client_secret = settings.GOOGLE_OAUTH2_CLIENT_SECRET
-        print(f"DEBUG 초기 값: {settings.DEBUG}")
-
-        if settings.DEBUG:
-            self.redirect_uri = "http://localhost:8000/api/accountapp/auth/callback/"
-            print(True)
-        else:
-            self.redirect_uri = "http://aiden-kwak.com/api/accountapp/auth/callback/"
-            print(False)
-
-        print(f"DEBUG 최종 값: {settings.DEBUG}, URI: {self.redirect_uri}")
+        self.redirect_uri = ("http://localhost:8000/api/accountapp/auth/callback/" if settings.DEBUG
+                             else "http://aiden-kwak.com/api/accountapp/auth/callback/")
         
     def _generate_state_token(self, length=30):
         rand = SystemRandom()
