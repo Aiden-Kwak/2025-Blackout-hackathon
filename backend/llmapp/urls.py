fea6dc690e1250307fdfe6725a428e0210d3762d
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-12 05:20:03 +0900

    답변생성 연결

--- backend/llmapp/urls.py
+++ backend/llmapp/urls.py
@@ -1,8 +1,8 @@
 # llmapp/urls.py
 from django.urls import path
-from .views import FetchSlackMessagesAPIView#, CreateEmbeddingsAPIView
+from .views import FetchAndGenerateSlackResponseAPIView
 
 urlpatterns = [
-    path("slack/fetch-messages/", FetchSlackMessagesAPIView.as_view(), name="fetch_slack_messages"),
+    path("slack/fetch-messages/", FetchAndGenerateSlackResponseAPIView.as_view(), name="fetch_slack_messages"),
     #path("slack/create-embeddings/", CreateEmbeddingsAPIView.as_view(), name="create_embeddings"),
 ]
