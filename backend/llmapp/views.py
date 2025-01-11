501f43df32eb765ac26aca3ff4326f3cad2e68fb
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-12 08:16:18 +0900

    shit_test

--- backend/llmapp/views.py
+++ backend/llmapp/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, permissions
 from django.shortcuts import render
 from .serializers import PostSerializer, CategorySerializer
